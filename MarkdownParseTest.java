e9015ea0e71d9632494572f625d55506fed21595
Neel Basu <neel95014@gmail.com>
2022-01-26 19:42:40 -0800

    Added JUnit Tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,25 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void testGetLinks() throws IOException {
+        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("test-file1.md")));
+        assertEquals(List.of("https://something.com", "some-page.html"), links);
+    }
 }
+
+/**
+ * Compile: javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+ * Run: java -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+ */
